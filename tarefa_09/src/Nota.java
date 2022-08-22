f028eafb379e543648967b4ca2769432e3fbc79b
Thiago Araujo <thiago.araujo@sinqia.com.br>
2022-08-22 11:04:34 -0300

    Tarefa_Hoepers

---
+++ tarefa_09/src/Nota.java
@@ -0,0 +1,22 @@
+public class Nota {
+    public static void main(String args[]) {
+        matematica();
+    }
+    private static void matematica() {
+        float primeiroBimestre = 9.8f;
+        double primeiroBimestre1 = primeiroBimestre;
+        float segundoBimestre  = 8.5f;
+        double segundoBimestre1  = segundoBimestre;
+        float terceiroBimestre = 9.7f;
+        double terceiroBimestre1 = terceiroBimestre;
+        float quartoBimestre   = 10f;
+        double quartoBimestre1   = quartoBimestre;
+
+
+        float mediaMatematica = (primeiroBimestre + segundoBimestre + terceiroBimestre + quartoBimestre) / 4;
+        System.out.println("Media da Fernanda em Matematica: " + mediaMatematica);
+
+        int idade =10;
+        long idadel = idade;
+    }
+}
